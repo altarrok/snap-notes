9a0a4db9d959980c0df643dcfe30c4f0d14b8488
Altay Batuhan <altaybatuhanmail@gmail.com>
2023-04-11 17:27:42 -0700

    add sort options

--- src/components/sort-selector/SortSelector.tsx
+++ src/components/sort-selector/SortSelector.tsx
@@ -1,36 +1,49 @@
-import { useState } from "react";
 import { Listbox } from '@headlessui/react'
-import { MdExpandMore, MdTag } from "react-icons/md";
-import React from "react";
+import { MdExpandMore, MdSort } from "react-icons/md";
+import React, { useMemo } from "react";
 
-export const TagFilter: React.FC<{
-    tags: string[],
-    selectedTags: string[],
-    onChange: (selectedTags: string[]) => void,
-}> = ({ tags, selectedTags, onChange }) => {
+export const SortSelector: React.FC<{
+    onChange: (sortBy: "TITLE" | "DATE") => void,
+}> = ({ onChange }) => {
+
+    const sortOptions: {
+        label: string,
+        option: "TITLE" | "DATE",
+    }[] = useMemo(() => {
+        return [
+            {
+                label: "By Date",
+                option: "DATE"
+            },
+            {
+                label: "By Title",
+                option: "TITLE"
+            }
+        ]
+    }, [])
 
     return (
         <div className="relative">
-            <Listbox value={selectedTags} onChange={onChange} multiple>
+            <Listbox defaultValue={sortOptions[0]} onChange={(sortOption) => onChange(sortOption.option)}>
                 <Listbox.Button>
                     <span className="text-2xl p-2 flex items-center border border-solid border-gray-400 rounded-md">
-                        <MdTag />
+                        <MdSort />
                         <MdExpandMore />
                     </span>
                 </Listbox.Button>
                 <Listbox.Options className="absolute right-0 z-10 bg-white border border-solid border-gray-300 mt-1 p-2 rounded-md max-h-96 max-w-lg overflow-y-auto text-right">
-                    {tags.map((tag, i) => (
+                    {sortOptions.map((sortOption, i) => (
                         <Listbox.Option
                             key={i}
-                            value={tag}
+                            value={sortOption}
                             as={React.Fragment}
                         >
                             {({ selected }) => (
                                 <li
-                                    title={tag}
+                                    title={sortOption.label}
                                     className={`min-w-max text-ellipsis overflow-x-hidden hover:bg-gray-200 transition-colors p-2 cursor-pointer rounded-md my-2 ${selected && 'bg-green-300 hover:bg-green-200'}`}
                                 >
-                                    {tag}
+                                    {sortOption.label}
                                 </li>
                             )}
                         </Listbox.Option>
